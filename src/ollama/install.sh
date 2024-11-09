a1b6a2f0a9dd220e580e4397e363c7855789d080
Alex Fraser <alex.d.fraser@gmail.com>
2024-11-09 07:56:54 +0000

    storing models in /usr/local/share

--- src/ollama/install.sh
+++ src/ollama/install.sh
@@ -4,6 +4,24 @@ set -e
 
 PULL=${PULL:-""}
 
+# From https://github.com/itsmechlark/features/blob/0e7c373cd84e8dfd6be03594815e4304855d3911/src/postgresql/install.sh#L24
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
+fi
+
 # Checks if packages are installed and installs them if not
 check_packages() {
 	if ! dpkg -s "$@" >/dev/null 2>&1; then
@@ -18,6 +36,8 @@ check_packages() {
 # make sure we have curl
 check_packages ca-certificates curl
 
+mkdir -p "$OLLAMA_MODELS"
+
 curl https://ollama.ai/install.sh | sh
 
 # if PULL is not empty and not equal to "none", also ollama is serving, then pull the models
@@ -27,5 +47,7 @@ if [ "$PULL" != "none" ] && [ "$PULL" != "" ]; then
     sleep 3
     echo $PULL | tr ',' '\n' | xargs -I % sh -c "ollama pull %"
 fi
+
+chown --preserve-root --recursive "${USERNAME}:root" "$OLLAMA_MODELS"
 
 echo 'Done!'
